316fa10c781c8892e9ae606f6f9f2bad74da1bf4
kalinkinfiz <kalinkin@DESKTOP-8DQSNBL.localdomain>
2020-10-04 03:05:21 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ exports.max = function max (array) {
 
 exports.avg = function avg (array) {
   if (array == undefined || array.length == 0) return 0;
-  return array.reduce( (s, i) => s + i, 0) / array.length
+  return array.reduce( (s, i) => s + i, 0) / array.length;
 }
